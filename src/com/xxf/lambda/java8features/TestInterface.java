30921b6eb48c20df8d32d40f5d1c6379b6b4fd1f
xxf <xuanyouwu@163.com>
2018-09-19 17:51:05 +0800

    java 8 funcationinterface注解

---
+++ src/com/xxf/lambda/java8features/TestInterface.java
@@ -0,0 +1,13 @@
+package com.xxf.lambda.java8features;
+
+/**
+ * com.xxf.lambda.java8features
+ * 2018/9/19
+ * author:asange
+ * email:xuanyouwu@163.com
+ **/
+@FunctionalInterface
+public interface TestInterface {
+
+    void onlyOneAbstractMethod();
+}
